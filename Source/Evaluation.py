c80cf162738b79e87ca8c212affc2c67ae16e234
haktanpolatt <101014945+haktanpolatt@users.noreply.github.com>
2025-05-26 20:00:21 +0200

    Update Evaluation.py
    Added Piece-Square Tables to evaluation function.

--- Source/Evaluation.py
+++ Source/Evaluation.py
@@ -4,15 +4,67 @@
 #                             #
 ###############################
 
+import chess
+from Source.Constants import values, phase_values, POSITION_WEIGHT
+from Source.PST import pawn_table, knight_table, bishop_table, rook_table, queen_table, king_table_opening, king_table_endgame
+
 def evaluate_board(board):
-    score = 0
-    values = {'P':1, 'N':3, 'B':3, 'R':5, 'Q':10, 
-              'p':-1, 'n':-3, 'b':-3, 'r':-5, 'q':-10}
-    
+    material_score = 0
+    position_score = 0
+    phase_score = 0
+
     pieces = board.piece_map()
     for square, piece in pieces.items():
         symbol = piece.symbol()
-        if symbol in values:
-            score += values[symbol]
+        s = symbol.lower()
 
-    return score
+        if symbol in values:
+            material_score += values[symbol]
+        
+        if s in phase_values:
+            phase_score += phase_values[s]
+
+        if s == 'p':
+            if piece.color == chess.WHITE:
+                pst_score = pawn_table[square]
+            else:
+                pst_score = -pawn_table[chess.square_mirror(square)]
+            position_score += pst_score
+        elif s == 'n':
+            if piece.color == chess.WHITE:
+                pst_score = knight_table[square]
+            else:
+                pst_score = -knight_table[chess.square_mirror(square)]
+            position_score += pst_score
+        elif s == 'b':
+            if piece.color == chess.WHITE:
+                pst_score = bishop_table[square]
+            else:
+                pst_score = -bishop_table[chess.square_mirror(square)]
+            position_score += pst_score
+        elif s == 'r':
+            if piece.color == chess.WHITE:
+                pst_score = rook_table[square]
+            else:
+                pst_score = -rook_table[chess.square_mirror(square)]
+            position_score += pst_score
+        elif s == 'q':
+            if piece.color == chess.WHITE:
+                pst_score = queen_table[square]
+            else:
+                pst_score = -queen_table[chess.square_mirror(square)]
+            position_score += pst_score
+        elif s == 'k':
+            if phase_score <= 14:
+                table = king_table_endgame
+            else:
+                table = king_table_opening
+
+            if piece.color == chess.WHITE:
+                pst_score = table[square]
+            else:
+                pst_score = -table[chess.square_mirror(square)]
+
+            position_score += pst_score
+
+    return material_score + POSITION_WEIGHT * position_score
