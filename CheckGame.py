f358d47cdd45e88ca9660f5d7b5d754d073ba599
haktanpolatt <101014945+haktanpolatt@users.noreply.github.com>
2025-05-26 13:44:53 +0200

    Update CheckGame.py
    The rich module was used in console outputs to increase visual readability.

--- CheckGame.py
+++ CheckGame.py
@@ -4,20 +4,22 @@
 #                             #
 ###############################
 
+from rich import print
+
 def check_game_over(board):
     if board.is_checkmate():
-        print("Checkmate! Game over.")
+        print("[bold red]Checkmate! Game over.[/bold red]")
         return True
     elif board.is_stalemate():
-        print("Game drawn by stalemate.")
+        print("[yellow]Game drawn by stalemate.[/yellow]")
         return True
     elif board.is_insufficient_material():
-        print("Game drawn due to insufficient material.")
+        print("[blue]Game drawn due to insufficient material.[/blue]")
         return True
     elif board.is_seventyfive_moves():
-        print("Draw by 75-move rule.")
+        print("[magenta]Draw by 75-move rule.[/magenta]")
         return True
     elif board.is_fivefold_repetition():
-        print("Draw by repetition.")
+        print("[cyan]Draw by repetition.[/cyan]")
         return True
     return False
