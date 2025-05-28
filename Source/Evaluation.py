f4ef7e5478997c45ffd809da1d5c564fae90f37a
haktanpolatt <101014945+haktanpolatt@users.noreply.github.com>
2025-05-28 15:27:02 +0200

    Update Evaluation.py
    Added mobility score function and pawn structure function to evaluation function.

--- Source/Evaluation.py
+++ Source/Evaluation.py
@@ -5,13 +5,90 @@
 ###############################
 
 import chess
-from Source.Constants import values, phase_values, POSITION_WEIGHT
+from collections import defaultdict
+from Source.Constants import values, phase_values, POSITION_WEIGHT, MOBILITY_WEIGHT
 from Source.PST import pawn_table, knight_table, bishop_table, rook_table, queen_table, king_table_opening, king_table_endgame
+
+def is_isolated_pawn(square, board):
+    file = chess.square_file(square)
+    rank = chess.square_rank(square)
+    piece = board.piece_at(square)
+    if not piece or piece.symbol().lower() != 'p':
+        return False 
+
+    is_white = piece.color == chess.WHITE
+    own_pawn = 'P' if is_white else 'p'
+
+    for df in [-1, 1]:
+        neighbor_file = file + df
+        if 0 <= neighbor_file <= 7:
+            for r in range(8):
+                neighbor_square = chess.square(neighbor_file, r)
+                neighbor_piece = board.piece_at(neighbor_square)
+                if neighbor_piece and neighbor_piece.symbol() == own_pawn:
+                    return False
+
+    return True
+
+def evaluate_pawn_structure(board):
+    total_pawn_score = 0
+    white_files = defaultdict(int)
+    black_files = defaultdict(int)
+    
+    for square, piece in board.piece_map().items():
+        if piece.symbol().lower() != 'p':
+            continue
+
+        is_white = piece.color == chess.WHITE
+        forward = 8 if is_white else -8
+        left_diag = 7 if is_white else -9
+        right_diag = 9 if is_white else -7
+        opponent_pawn = 'p' if is_white else 'P'
+
+        passed = True
+        for offset in [forward, left_diag, right_diag]:
+            target = square + offset
+            if 0 <= target < 64:
+                target_piece = board.piece_at(target)
+                if target_piece and target_piece.symbol() == opponent_pawn:
+                    passed = False
+                    break
+
+        if passed:
+            total_pawn_score += 0.3
+
+        if is_isolated_pawn(square, board):
+            total_pawn_score -= 0.2
+
+        file = chess.square_file(square)
+        if is_white:
+            white_files[file] += 1
+        else:
+            black_files[file] += 1
+
+    for file in white_files:
+        if white_files[file] > 1:
+            total_pawn_score -= 0.1 * (white_files[file] - 1)
+    for file in black_files:
+        if black_files[file] > 1:
+            total_pawn_score += 0.1 * (black_files[file] - 1)
+
+    return total_pawn_score
+
+def evaluate_mobility(board):
+    white_moves = len(list(board.legal_moves))
+    board.push(chess.Move.null())
+    black_moves = len(list(board.legal_moves))
+    board.pop()
+
+    return white_moves - black_moves
 
 def evaluate_board(board):
     material_score = 0
     position_score = 0
     phase_score = 0
+    mobility_score = evaluate_mobility(board)
+    pawn_structure_score = evaluate_pawn_structure(board)
 
     pieces = board.piece_map()
     for square, piece in pieces.items():
@@ -67,4 +144,4 @@ def evaluate_board(board):
 
             position_score += pst_score
 
-    return material_score + POSITION_WEIGHT * position_score
+    return material_score + POSITION_WEIGHT * position_score + MOBILITY_WEIGHT * mobility_score + pawn_structure_score
